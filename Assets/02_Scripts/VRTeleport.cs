939272b022b7dfab166f06c4982c92b3f6aa9b49
RachelChao <drawinsky@gmail.com>
2023-04-20 10:54:02 -0700

    update teleport (Grip)

--- Assets/02_Scripts/VRTeleport.cs
+++ Assets/02_Scripts/VRTeleport.cs
@@ -1,38 +1,40 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 // from class demo: VR Teleport  notes
 public class VRTeleport : MonoBehaviour
 {
     public Transform head;
     public Transform hand;
-
-    private VRInputController input;
-    public bool teleporting = false;
-
+    public bool Teleport;
+    public Vector3 TeleportTarget;
+    //private VRInputController input;
+    private VRInputActions vrInputActions;
     private void Awake()
     {
-        input = GetComponent<VRInputController>();
+        //input = GetComponent<VRInputController>();
+        vrInputActions = new VRInputActions();
+        vrInputActions.Enable();
     }
-
     private void Update()
     {
         if (Physics.Raycast(hand.position, hand.forward, out RaycastHit hit))
         {
-            if (input.RightTriggerPressed)
-            {
-                Vector3 directionToHead = transform.position - head.position;
-                directionToHead.y = 0;
-                transform.position = hit.point + directionToHead;
-                teleporting = true;
-            }
+            TeleportTarget = hit.point;
         }
-
-        Debug.DrawLine(transform.position, head.position, Color.cyan, 0);
-        Debug.DrawLine(transform.position, hit.point, Color.yellow, 0);
-
-        if (teleporting)
+        Vector3 directionToHead = transform.position - head.position;
+        directionToHead.y = 0;
+        // If the user presses the trigger? If they do *something*.
+        // They point the controller somewhere, getting a
+        // location they want to move to.
+        if (vrInputActions.Default.RightGrip.WasPerformedThisFrame())
+        //if (input.RightTriggerPressed)
         {
-            Debug.DrawRay(transform.position, Vector3.up * 2, Color.green);
+            // Teleport the...rig? To the target position.
+            transform.position = TeleportTarget + directionToHead;
+            Teleport = false;
         }
+        Debug.DrawLine(transform.position, head.position, Color.cyan, 0);
+        Debug.DrawLine(transform.position, TeleportTarget, Color.yellow, 0);
+        Debug.DrawRay(TeleportTarget, directionToHead, Color.red, 0);
     }
 }
